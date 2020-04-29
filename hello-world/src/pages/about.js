fe74cccb98d887a7f2041f3dcd379816bb53fa94
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-04-29 14:40:03 -0500

    added new components to src

---
+++ hello-world/src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from '../components/header'
+
+export default () => (
+    <div style={{ color: `teal` }}>
+        <Header headerText='About dezz nuts'/>
+        <Header headerText='suck them' />
+        <p>Such wow. Very React.</p>
+    </div>
+)
--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <div style={{color: 'purple'}}>
+        <h1>Hello Daddy!</h1>
+        <p>come to me.</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+)
