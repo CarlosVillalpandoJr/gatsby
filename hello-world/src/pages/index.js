2e9e20538f126a0ce855bd22c9f112db7be75167
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-04-29 14:47:44 -0500

    completed basic tutorial minus the deployment

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 export default () => (
     <div style={{color: 'purple'}}>
+        <Link to='/contact/'>Contact</Link>
         <h1>Hello Daddy!</h1>
         <p>come to me.</p>
         <img src="https://source.unsplash.com/random/400x200" alt="" />
